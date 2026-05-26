3a8e575ded89c210026a9a5661d4f0eb1f069dbb
kshitij Sharma <Ksharma199@protonmail.com>
2026-05-26 18:22:53 +0530

    Updated config file to fix vercel deployment issue

--- server/config/config.js
+++ server/config/config.js
@@ -4,9 +4,9 @@ dotenv.config({path: "/home/kshitij/Desktop_Content/Hotel_Booking_App/server/.en
 const config = {
     MONGODB_URI : process.env.MONGODB_URI,
     PORT : process.env.PORT,
-    CLERK_PUBLISHABLE_KEY=process.env.CLERK_PUBLISHABLE_KEY,
-    CLERK_SECRET_KEY=process.env.CLERK_SECRET_KEY,
-    CLERK_WEBHOOKS_SECRET=process.env.CLERK_WEBHOOKS_SECRET
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY:process.env.CLERK_SECRET_KEY,
+    CLERK_WEBHOOKS_SECRET:process.env.CLERK_WEBHOOKS_SECRET
 }
 
 export default config;
