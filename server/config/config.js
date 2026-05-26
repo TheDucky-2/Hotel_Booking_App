9b60ec8de12d8ec0989f575e8ac0309ea7c51bc7
kshitij Sharma <Ksharma199@protonmail.com>
2026-05-26 18:15:52 +0530

    Updated the bug causing error 500 during deployment on vercel

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,6 @@ dotenv.config({path: "/home/kshitij/Desktop_Content/Hotel_Booking_App/server/.en
 const config = {
     MONGODB_URI : process.env.MONGODB_URI,
     PORT : process.env.PORT,
-    PORT=3000,
     CLERK_PUBLISHABLE_KEY=process.env.CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY=process.env.CLERK_SECRET_KEY,
     CLERK_WEBHOOKS_SECRET=process.env.CLERK_WEBHOOKS_SECRET
