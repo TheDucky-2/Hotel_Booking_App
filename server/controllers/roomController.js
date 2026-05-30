defe9f5ad177da6b8163529deeb69d72284b17db
kshitij Sharma <Ksharma199@protonmail.com>
2026-05-30 16:33:09 +0530

    Debug the issue causing list rooms to not show available rooms listed by the hotel owner

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -8,7 +8,7 @@ export const createRoom = async(req, res) => {
     try{
 
         const {roomType, pricePerNight, amenities} = req.body;
-        const hotel = await Hotel.findOne({owner: req.auth.userId})
+        const hotel = await Hotel.findOne({owner: req.user._id})
 
         if(!hotel){
             return res.json({
@@ -52,7 +52,7 @@ export const getRooms = async(req, res) => {
 
     try{
 
-        await Room.find({isAvailable:true}).populate({
+        const rooms = await Room.find({isAvailable:true}).populate({
             path: 'hotel',
             populate: {
                 path: 'owner',
@@ -80,7 +80,7 @@ export const getOwnerRooms = async(req, res) => {
 
         try{
 
-            const hotelData = await Hotel({owner: req.auth.userId})
+            const hotelData = await Hotel.findOne({owner: req.user._id})
 
             const rooms = await Room.find({hotel: hotelData._id.toString()}).populate("hotel");
 
